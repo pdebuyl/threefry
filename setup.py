7684fcc925d4b3cff8b04a83824c7cea8305dc72
Pierre de Buyl <pdebuyl@pdebuyl.be>
2017-05-06 14:35:55 +0200

    add c99 compile flag + pep8/flake8 setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,17 @@ from Cython.Build import cythonize
 VERSION = '0.1.0.dev1'
 
 threefry_ext = cythonize(Extension("*",
-                     sources=["threefry/threefry.pyx", "threefry/_threefry.c"],),
-                     include_path=["threefry"],
-                     )
+                                   sources=["threefry/threefry.pyx",
+                                            "threefry/_threefry.c"],
+                                   extra_compile_args=['-std=c99'],
+                                   ),
+                         include_path=["threefry"],
+                         )
+
 with open('threefry/version.py', 'w') as version_f:
-    version_f.write("__version__ = '{__version__}'".format(__version__=VERSION))
+    version_f.write(
+        "__version__ = '{__version__}'".format(__version__=VERSION)
+    )
 
 
 setup(name='threefry',
@@ -19,5 +25,6 @@ setup(name='threefry',
       packages=['threefry'],
       ext_modules=threefry_ext,
       setup_requires=['cython'],
-      package_data={'threefry': ['__init__.pxd', 'threefry.pxd', 'threefry.h']},
+      package_data={'threefry': ['__init__.pxd',
+                                 'threefry.pxd', 'threefry.h']},
       )
